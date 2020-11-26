7c7d64bbd1b05ea7de86593ea362777f66e3e437
UnityDo <63395346+UnityDo@users.noreply.github.com>
2020-11-26 12:24:50 +0100

    VisualizaConstructor va con ConstructorLadrillos

---
+++ Editor/VisualizaConstructor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Constructor))]
+public class VisualizaConstructor : Editor
+{
+    //Modificar como pinta el inspector
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        Constructor Mitarget = (Constructor)target;
+        
+        if(GUILayout.Button("Crea con Imagen"))
+        {
+            Mitarget.CreaConImagen();
+        }
+        if (GUILayout.Button("Destruye Ladrillos"))
+        {
+            Mitarget.DestruyeLadrillos();
+        }
+    }
+
+}
