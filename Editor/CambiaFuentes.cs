c21382e4ddab5b8ef07e814ea48cfa6beed42b75
UnityDo <63395346+UnityDo@users.noreply.github.com>
2020-11-27 11:08:22 +0100

    Cambia todas las fuentes de la escena

---
+++ Editor/CambiaFuentes.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using TMPro;
+
+public class CambiaFuentes : EditorWindow
+{
+    TMP_FontAsset fuente;
+    [MenuItem("Window/Cambia Fuentes")]
+    static void Init()
+    {
+        CambiaFuentes ventana = (CambiaFuentes)EditorWindow.GetWindow(typeof(CambiaFuentes));
+        ventana.Show();
+    }
+    private void OnGUI()
+    {
+
+        fuente = (TMP_FontAsset)EditorGUILayout.ObjectField(fuente, typeof(TMP_FontAsset), true, GUILayout.Width(200));
+        if (GUILayout.Button("Cambia Fuentes"))
+        {
+            TextMeshProUGUI[] Textos = GameObject.FindObjectsOfType<TextMeshProUGUI>();
+
+            //Cambiar todas las fuentes
+            foreach(TextMeshProUGUI t in Textos)
+            {
+                t.font = fuente;
+                t.UpdateFontAsset();
+            }
+
+        }
+    }
+}
